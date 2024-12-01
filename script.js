7ba004996b5d0e82b79c29f6db8e09b45f5f8575
subhashinisai <subhashinisaipriyatripathi@gmail.com>
2024-12-02 00:29:46 +0530

    js started

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputBox= document.querySelector('.input-box');
+const searchBtn= document.getElementById('searchBtn');
+const weather_img= document.getElementById('.weather-img');
+const temperature= document.querySelector('.temperature');
+const description= document.querySelector('.description');
+const humidity= document.getElementById('humidity');
+const wind_speed= document.getElementById('wind-speed');
