791e90379175e980250028083bd21e3e4ded56d8
subhashinisai <subhashinisaipriyatripathi@gmail.com>
2024-12-03 00:12:52 +0530

    all done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputBox= document.querySelector('.input-box');
 const searchBtn= document.getElementById('searchBtn');
-const weather_img= document.getElementById('.weather-img');
+const weather_img= document.querySelector('.weather-img');
 const temperature= document.querySelector('.temperature');
 const description= document.querySelector('.description');
 const humidity= document.getElementById('humidity');
